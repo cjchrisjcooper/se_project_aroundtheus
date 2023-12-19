f28cbb1bcfd31703cdfe840f321e7063a7d3d74b
Chris Cooper <cjchrisjcooper@gmail.com>
2023-12-19 15:00:23 -0700

    refactor: UserInfo.js 2nd time

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,9 +1,7 @@
 export default class UserInfo {
   constructor(nameElement, jobElement) {
-    this._name = nameElement;
-    this._job = jobElement;
-    this.profileJobEl = document.querySelector(this._job);
-    this.profileNameEl = document.querySelector(this._name);
+    this.profileJobEl = document.querySelector(jobElement);
+    this.profileNameEl = document.querySelector(nameElement);
   }
 
   getUserInfo() {
@@ -12,17 +10,13 @@ export default class UserInfo {
       name: "",
       job: "",
     };
-    newObj.name = document.querySelector(`${this._name}`).textContent;
-    newObj.job = document.querySelector(`${this._job}`).textContent;
-
+    newObj.name = this.profileNameEl.textContent;
+    newObj.job = this.profileJobEl.textContent;
     return newObj;
   }
 
   setUserInfo(name, job) {
-    this.profileName = name;
-    this.profileJob = job;
-
-    this.profileJobEl.textContent = this.profileJob;
-    this.profileNameEl.textContent = this.profileName;
+    this.profileJobEl.textContent = job;
+    this.profileNameEl.textContent = name;
   }
 }
